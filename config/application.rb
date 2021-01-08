147febd56311962fa90872e78bc4ec7206254015
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-01-08 16:17:46 +0900

    Add: テストフレームワークでRSpecを使用

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework :rspec, view_specs: false, routing_specs: false, helper_specs: false
+    end
   end
 end
