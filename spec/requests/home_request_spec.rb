d073361fb53c54db53b0d8d438c5566428e18c4d
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-01-09 11:08:29 +0900

    Add: トップページの作成

---
+++ spec/requests/home_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :request do
+
+  describe "GET /" do
+    before do
+      get root_path
+    end
+
+    it "responds successfully" do
+      expect(response).to have_http_status 200
+    end
+
+    it "render template home/index" do
+      expect(response).to render_template :index
+    end
+  end
+
+end
