8c2372d5cbeda652af7763938aacb809ec17843b
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-01-12 22:29:38 +0900

    Add: サインアップのフィーチャースペックを追加

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "Sign Up", type: :feature do
+  let(:user) { FactoryBot.build(:user) }
+
+  scenario "user sign up successfully" do
+    visit root_path
+    expect(page).to have_content "ユーザー登録"
+    click_link "ユーザー登録"
+    expect(page).to have_current_path(new_user_registration_path)
+    expect {
+      fill_in "Name", with: user.name
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      fill_in "Password confirmation", with: user.password
+      click_button "Sign up"
+      expect(page).to have_current_path(root_path)
+    }.to change(User, :count).by(1)
+  end
+
+  scenario "user fail to sign up" do
+    visit new_user_registration_path
+    expect {
+      fill_in "Name", with: "  "
+      fill_in "Email", with: "  "
+      fill_in "Password", with: "   "
+      fill_in "Password confirmation", with: "   "
+      click_button "Sign up"
+      expect(page).to have_current_path "/users"
+      # エラーメッセージ
+      expect(page).to have_content "Email can't be blank"
+      expect(page).to have_content "Password can't be blank"
+      expect(page).to have_content "Name can't be blank"
+    }.to_not change(User, :count)
+  end
+end
