0039ba3ba8f6dac7fd9459d0874558267fe52ba3
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-01-13 19:14:17 +0900

    Add: フラッシュメッセージの追加、BootStrapに対応させた

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
       "#{page_title} - #{base_title}"
     end
   end
+
+  def replace_flash_class(flash_class)
+    return unless flash_class.is_a?(String)
+
+    replacement = {
+      notice: "alert alert-success",
+      alert: "alert alert-warning",
+      error: "alert alert-danger"
+    }
+    replacement[flash_class.intern]
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -25,4 +25,33 @@ RSpec.describe ApplicationHelper do
       end
     end
   end
+
+  describe "#replace_flash_class" do
+    context "with expected argument" do
+      it "returns replaced class name" do
+        expect(replace_flash_class("notice")).to eq "alert alert-success"
+        expect(replace_flash_class("alert")).to eq "alert alert-warning"
+        expect(replace_flash_class("error")).to eq "alert alert-danger"
+      end
+    end
+
+    context "with unexpected argument" do
+      it "returns nil" do
+        expect(replace_flash_class("foo")).to eq nil
+        expect(replace_flash_class(1)).to eq nil
+      end
+    end
+
+    context "with empty argument" do
+      it "returns nil" do
+        expect(replace_flash_class("   ")).to eq nil
+      end
+    end
+
+    context "with nil argument" do
+      it "returns nil" do
+        expect(replace_flash_class(nil)).to eq nil
+      end
+    end
+  end
 end
