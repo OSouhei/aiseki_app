2d0c0c95723c8337fdb121ca72effc03ca06110c
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-01-14 23:42:09 +0900

    Add: ユーザー消去のテストを追加

---
+++ spec/features/destroy_user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "DestroyUsers", type: :feature do
+  let(:user) { FactoryBot.create(:user) }
+
+  scenario "user destroy his account" do
+    sign_in user
+    visit root_path
+    expect {
+      click_link "退会"
+    }.to change(User, :count).by(-1)
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_content "Bye! Your account has been successfully cancelled. We hope to see you again soon."
+  end
+end
