345380a94759506631ca429ecc9ecdb8072a8fa6
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-01-16 17:38:52 +0900

    Update: users/showでユーザの情報を表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do
-    resources :rooms
-  end
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions"
   }
+  resources :users, only: [:index, :show] do
+    resources :rooms
+  end
   root "home#index"
 end
