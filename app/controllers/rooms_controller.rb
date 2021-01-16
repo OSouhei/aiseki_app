6bbb00053d761bdf0e28a2cd9f5103273ab1f6c4
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-01-16 16:51:47 +0900

    Add: RoomsController#createを作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,16 +1,48 @@
 class RoomsController < ApplicationController
-  before_action :set_user, only: [:new]
-  before_action :authenticate_user!, only: [:new]
+  before_action :set_user, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :correct_user?, only: [:new, :create]
+  before_action :parse_params, only: [:create]
 
   def new
-    redirect_to root_path and return unless current_user? @user
-
     @room = @user.rooms.build
+  end
+
+  def create
+    @room = @user.rooms.build(
+      conditions: @conditions,
+      date: @date,
+      people_limit: @people_limit
+    )
+    if @room.save
+      flash[:notice] = "room was successfully created."
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def correct_user?
+    redirect_to root_path and return unless current_user? @user
+  end
+
+  def parse_params
+    room_params = params[:room]
+    @conditions = room_params[:conditions]
+    @people_limit = room_params[:people_limit]
+    @date = room_params[:date]
+    @date = Time.zone.local(
+      room_params["date(1i)"],
+      room_params["date(2i)"],
+      room_params["date(3i)"],
+      room_params["date(4i)"],
+      room_params["date(5i)"]
+    )
   end
 end
--- spec/requests/rooms_request_spec.rb
+++ spec/requests/rooms_request_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe "Rooms", type: :request do
   let(:user) { FactoryBot.create(:user) }
   let(:other_user) { FactoryBot.create(:user) }
+  let(:room_params) do
+    FactoryBot.attributes_for(:room).merge!({ "date(1i)": 2021, "date(2i)": 12, "date(3i)": 11, "date(4i)": 15, "date(5i)": 45 })
+  end
 
   describe "GET /users/:user_id/rooms/new" do
     context "when authenticated user" do
@@ -46,6 +49,70 @@ RSpec.describe "Rooms", type: :request do
 
       it "redirect to login page" do
         expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe "POST /users/:user_id/rooms" do
+    context "when authenticated user" do
+      before do
+        sign_in user
+      end
+
+      it "responds successfully" do
+        post user_rooms_path(user), params: { room: room_params }
+        expect(response).to have_http_status 302
+      end
+
+      it "redirect to home page" do
+        post user_rooms_path(user), params: { room: room_params }
+        expect(response).to redirect_to root_path
+      end
+
+      it "create room" do
+        expect {
+          post user_rooms_path(user), params: { room: room_params }
+        }.to change(Room, :count).by(1)
+      end
+    end
+
+    context "when unauthenticated user" do
+      before do
+        sign_in other_user
+      end
+
+      it "responds successfully" do
+        post user_rooms_path(user), params: { room: room_params }
+        expect(response).to have_http_status 302
+      end
+
+      it "redirect to home page" do
+        post user_rooms_path(user), params: { room: room_params }
+        expect(response).to redirect_to root_path
+      end
+
+      it "does not create room" do
+        expect {
+          post user_rooms_path(user), params: { room: room_params }
+        }.to_not change(Room, :count)
+      end
+    end
+
+    context "when guest" do
+      it "responds successfully" do
+        post user_rooms_path(user), params: { room: room_params }
+        expect(response).to have_http_status 302
+      end
+
+      it "redirect to login page" do
+        post user_rooms_path(user), params: { room: room_params }
+        expect(response).to redirect_to new_user_session_path
+      end
+
+      it "does not create room" do
+        expect {
+          post user_rooms_path(user), params: { room: room_params }
+        }.to_not change(Room, :count)
       end
     end
   end
