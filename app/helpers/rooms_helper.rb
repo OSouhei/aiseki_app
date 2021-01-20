6f233bffa732027008051e5c19a4724a3c4de62d
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-01-20 17:32:46 +0900

    Add: RoomsHelper#search_shopを作成
      APIを使用して店名を検索するメソッド

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,28 @@
+require 'json'
+require 'net/https'
+require "uri"
+
 module RoomsHelper
+  def search_shops(term)
+    data = {
+      key: "a4510e393d79acc0",
+      keyword: term
+    }
+    query = data.to_query
+    uri = URI("https://webservice.recruit.co.jp/hotpepper/shop/v1/?#{query}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(uri)
+    res = http.request(req)
+    Hash.from_xml(res.body)
+  end
+
+  def parse_json(data)
+    shops = data["results"]["shop"]
+    shop_names = []
+    shops.each do |shop|
+      shop_names << shop["name"]
+    end
+    shop_names
+  end
 end
