1cade199b511045951ad86b3fdff4a9b1aa698e7
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-01-24 22:41:22 +0900

    Add: roomを削除する際のシナリオを作成

---
+++ spec/system/destroy_rooms_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "DestroyRooms", type: :system do
+  let(:user) { FactoryBot.create(:user) }
+  let(:other_user) { FactoryBot.create(:user) }
+  let(:room) { FactoryBot.create(:room, user: user) }
+
+  before do
+    @room = FactoryBot.create(:room, user: user)
+  end
+
+  scenario "user destroy his room" do
+    sign_in user
+    visit user_path(user)
+    expect(page).to have_current_path user_path(user)
+    find("#room#{@room.id}").click
+    expect(page).to have_current_path room_path(@room)
+    expect(page).to have_content @room.conditions
+    expect(page).to have_content @room.date
+    expect(page).to have_content @room.people_limit
+    expect(page).to have_link "edit this room"
+    expect(page).to have_link "destroy this room"
+    expect {
+      click_link "destroy this room"
+    }.to change(Room, :count).by(-1)
+  end
+
+  scenario "unauthenticated user can not destroy his room" do
+    sign_in other_user
+    visit user_path(user)
+    find("#room#{@room.id}").click
+    expect(page).to have_current_path room_path(@room)
+    expect(page).to_not have_link "edit this room"
+    expect(page).to_not have_link "destroy this room"
+  end
+end
