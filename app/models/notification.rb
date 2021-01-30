15a4db7ba183d38f73f11a3fefe89f1d0acb6dfb
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-01-30 13:23:31 +0900

    Add: 通知一覧ページを作成

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,12 +1,15 @@
 class Notification < ApplicationRecord
   belongs_to :notify_to, class_name: "User", foreign_key: :to
   belongs_to :notifyed_by, class_name: "User", foreign_key: :by
-  belongs_to :room
 
   validates :to, presence: true, numericality: { only_integer: true }
   validates :by, presence: true, numericality: { only_integer: true }
   # 通知を受け取るユーザーと通知の対象の部屋の所持者は一致する
   validate :room_owner_and_notify_to_are_the_same
+
+  def room
+    Room.find_by(id: room_id)
+  end
 
   def room_owner_and_notify_to_are_the_same
     return unless room = Room.find_by(id: room_id)
---
+++ spec/system/notifications_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Notifications", type: :system do
+  let(:user) { FactoryBot.create(:user) }
+  let(:user1) { FactoryBot.create(:user) }
+  let!(:user2) { FactoryBot.create(:user) }
+  let(:room) { FactoryBot.create(:room, owner: user) }
+
+  before do
+    driven_by(:rack_test)
+  end
+
+  scenario "the page has notifications" do
+    user1.join room
+    sign_in user
+    visit root_path
+    click_link "Notifications"
+    expect(page).to have_current_path notifications_path
+    expect(page).to have_link(user1.name, href: user_path(user1))
+    expect(page).to_not have_link href: user_path(user2)
+    expect(page).to have_link("your room - #{room.title}", href: room_path(room))
+  end
+
+  scenario "guest access notificatons page" do
+    visit notifications_path
+    expect(page).to have_current_path(new_user_session_path)
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    expect(page).to have_current_path(notifications_path)
+  end
+end
