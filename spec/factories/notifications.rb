78d6df0aedea5ace098822a1085fb5d0ad2d0852
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-01-30 14:18:48 +0900

    Fix: FactoryBot :notification で関連するroomを作成

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :notification do
     to { FactoryBot.create(:user).id }
     by { FactoryBot.create(:user).id }
-    room_id { FactoryBot.create(:room, owner: notify_to) }
+    room_id { FactoryBot.create(:room, owner: notify_to).id }
     checked { false }
   end
 end
