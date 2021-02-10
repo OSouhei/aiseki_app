842f9df53c9cb5e6714dd0fab753787868b51914
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-02-10 18:38:25 +0900

    Update: ログインのシステムスペックを作成

--- spec/system/sign_in_spec.rb
+++ spec/system/sign_in_spec.rb
@@ -6,40 +6,31 @@ RSpec.feature "Sign In", type: :system do
   scenario "sign in page have right title" do
     visit new_user_session_path
     expect(page).to have_title "Login - 相席app"
+    expect(page).to have_current_path new_user_session_path
   end
 
-  xscenario "user sign in and sign out successfully" do
-    visit root_path
-    click_link "サインイン"
-    expect(page).to have_current_path(new_user_session_path)
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "Signed in successfully."
-    expect(page).to_not have_content "ログイン"
-    expect(page).to_not have_content "ユーザー登録"
-    expect(page).to have_content "ログアウト"
+  scenario "user sign in and sign out" do
+    visit new_user_session_path
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+    expect(page).to have_current_path root_path
+    # サインイン後のスペックを追加…
+    # ログアウト
+    expect(page).to have_link "ログアウト"
     click_link "ログアウト"
-    expect(page).to have_current_path(root_path)
+    expect(page).to have_current_path root_path
     expect(page).to have_content "Signed out successfully."
-    expect(page).to have_content "ユーザー登録"
-    expect(page).to have_content "ログイン"
-    expect(page).to_not have_content "ログアウト"
+    expect(page).to have_link "サインアップ"
+    expect(page).to have_link "サインイン"
   end
 
-  scenario "user fail to sign in" do
+  scenario "user fill in form wrong params" do
     visit new_user_session_path
-    fill_in "Email", with: "   "
-    fill_in "Password", with: "   "
-    click_button "Log in"
-    expect(page).to have_current_path(user_session_path)
-  end
-
-  scenario "user has already signed in" do
-    sign_in user
-    visit new_user_session_path
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "You are already signed in."
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: "foo"
+    click_button "ログイン"
+    expect(page).to have_current_path new_user_session_path
+    expect(page).to have_content "Invalid Email or password."
   end
 end
