9c9a5cdc411c8bbca36222a84c6e321e993ea8b0
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-02-10 16:42:35 +0900

    Update: 自分自身をフォローできないバリデーションを作成

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,4 +4,10 @@ class Relationship < ApplicationRecord
 
   validates :follower_id, presence: true, numericality: { only_integer: true }, uniqueness: { scope: :followed_id }
   validates :followed_id, presence: true, numericality: { only_integer: true }
+  # 自身をフォローできない
+  validate :follower_not_equal_followed
+
+  def follower_not_equal_followed
+    errors.add(:follower_id, "must not be equal to :followed_id") if follower_id == followed_id
+  end
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -11,6 +11,13 @@ RSpec.describe Relationship, type: :model do
       it { should validate_presence_of :follower_id }
       it { should validate_numericality_of(:follower_id).only_integer }
       it { should validate_uniqueness_of(:follower_id).scoped_to(:followed_id) }
+
+      # 自分をフォローできない
+      it "not equal to followed_id" do
+        relationship = build(:relationship, follower_id: 1, followed_id: 1)
+        relationship.valid?
+        expect(relationship.errors[:follower_id]).to include "must not be equal to :followed_id"
+      end
     end
 
     context "followed_id" do
