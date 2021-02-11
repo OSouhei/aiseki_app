8fd466d2688953bfd7b155f4405e88bccc1a1fca
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-02-12 02:49:09 +0900

    Update: トップページのデザイン変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @rooms = if user_signed_in?
-               current_user.joining.all
-             else
-               Room.take(6)
-             end
+    @rooms = Room.take 6
   end
 end
