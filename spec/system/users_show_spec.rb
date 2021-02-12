205e526a8db0006eec6b0a8f2689ae68e5e580f0
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-02-12 14:26:35 +0900

    Fix: users/showのシステムスペックを修正

--- spec/system/users_show_spec.rb
+++ spec/system/users_show_spec.rb
@@ -1,54 +1,48 @@
 require 'rails_helper'
 
-RSpec.feature "UsersShows", type: :system do
+RSpec.feature "users/show", type: :system do
   let(:user) { FactoryBot.create(:user) }
   let(:other_user) { FactoryBot.create(:user) }
+  let(:john) { create(:user) }
 
   scenario "has page title" do
     sign_in user
     visit root_path
-    click_link "プロフィール"
+    click_link href: user_path(user)
     expect(page).to have_title "#{user.name} - 相席app"
+    expect(page).to have_current_path user_path(user)
   end
 
-  scenario "has interface" do
+  scenario "has UI", js: true do
+    create_list(:room, 3, owner: user)
+    user.follow other_user
+    user.follow john
     sign_in user
-    visit root_path
-    click_link "プロフィール"
-    expect(page).to have_current_path(user_path(user))
-    expect(page).to have_content user.name
-    expect(page).to have_content user.email
-    expect(page).to have_link "編集"
+    visit user_path(user)
+    rooms = user.rooms
+    rooms.each do |room|
+      # ユーザーのルームの情報を持っているか
+      expect(page).to have_content room.title
+      expect(page).to have_content room.content
+      expect(page).to have_content room.shop_name
+      expect(page).to have_link "見る", href: room_path(room)
+      expect(page).to have_link "入室", href: join_room_path(room)
+      expect(page).to have_content room.members.count
+      expect(page).to have_content room.booked.count
+      expect(page).to have_link "編集", href: edit_room_path(room)
+    end
+    click_link "following"
+    following_users = user.following
+    following_users.each do |following|
+      # フォロー中のユーザーの情報を持っているか
+      expect(page).to have_content following.name
+      expect(page).to have_link href: user_path(following)
+    end
   end
 
   scenario "does not have link to edit user page" do
     sign_in other_user
     visit user_path(user)
-    expect(page).to_not have_link "編集"
-  end
-
-  scenario "has user's rooms content" do
-    10.times do |n|
-      FactoryBot.create(:room, content: "this is user's room#{n}", owner: user)
-      FactoryBot.create(:room, content: "this is other_user's room#{n}", owner: other_user)
-    end
-    sign_in user
-    visit root_path
-    click_link "プロフィール"
-    # ユーザーの部屋の情報を持っている
-    user.rooms.each do |room|
-      expect(page).to have_content room.content
-    end
-    # 他ユーザーの部屋の情報は持たない
-    other_user.rooms.each do |room|
-      expect(page).to_not have_content room.content
-    end
-  end
-
-  scenario "user is not found" do
-    sign_in user
-    visit user_path(user.id + 1000)
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "user is not found."
+    expect(page).to_not have_link "編集", href: edit_user_registration_path(user)
   end
 end
