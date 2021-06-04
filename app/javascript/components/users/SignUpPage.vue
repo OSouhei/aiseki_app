a70122ddb93a618d1d22804f2550064fcc6ff6ca
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-06-04 17:09:11 +0900

    Update: ユーザ登録のフォームにフロントエンド側でのバリデーションを実装

--- app/javascript/components/users/SignUpPage.vue
+++ app/javascript/components/users/SignUpPage.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="signup">
     <h2>アカウント登録</h2>
-    <el-form ref="form" :model="user" label-position="right" label-width="150px">
-      <el-form-item label="名前">
+    <el-form ref="form" :model="user" :rules="rules" label-position="right" label-width="150px">
+      <el-form-item label="名前" prop="name">
         <el-input v-model="user.name"></el-input>
       </el-form-item>
-      <el-form-item label="メールアドレス">
+      <el-form-item label="メールアドレス" prop="email">
         <el-input v-model="user.email"></el-input>
       </el-form-item>
-      <el-form-item label="パスワード">
-        <el-input v-model="user.password"></el-input>
+      <el-form-item label="パスワード" prop="password">
+        <el-input v-model="user.password" show-password></el-input>
       </el-form-item>
-      <el-form-item label="パスワード（確認）">
-        <el-input v-model="user.password_confirmation"></el-input>
+      <el-form-item label="パスワード（確認）" prop="password_confirmation">
+        <el-input v-model="user.password_confirmation" show-password></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">登録</el-button>
@@ -24,12 +24,46 @@
 <script>
 export default {
   data() {
+    // メールアドレスのカスタムバリデーション関数
+    var validateEmail = (rule, value, callback) => {
+      const validEmailRegex = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/
+      if(value === '') {
+        callback(new Error('メールアドレスを入力して下さい'))
+      // メールアドレスが正規表現にマッチする時
+      } else if (validEmailRegex.test(value)) {
+        callback()
+      // メールアドレスが正規表現にマッチしない時
+      } else {
+        callback(new Error('正しい形式のメールアドレスを入力して下さい'))
+      }
+    }
+
     return {
       user: {
         name: '',
         email: '',
         password: '',
         password_confirmation: '',
+      },
+      // 検証ルール
+      rules: {
+        name: [
+          { required: true, message: '名前を入力して下さい', trigger: 'blur' },
+          { max: 50, message: '名前は５０文字以内にして下さい', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: 'メールアドレスを入力して下さい', trigger: 'blur' },
+          { max: 255, message: 'メールアドレスは255文字以内にして下さい', trigger: 'blur' },
+          { validator: validateEmail, trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: 'パスワードを入力して下さい', trigger: 'blur' },
+          { min: 6, message: 'パスワードは６文字以上にして下さい', trigger: 'blur' }
+        ],
+        password_confirmation: [
+          { required: true, message: 'パスワードを入力して下さい', trigger: 'blur' },
+          { min: 6, message: 'パスワードは６文字以上にして下さい', trigger: 'blur' }
+        ]
       }
     }
   },
