6ed847969b1c666095c1881f63b74fe627a7cb8e
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-06-15 01:15:57 +0900

    Remove: rspecのシステムスペックを削除
      Vueをテストできない。ドライバーでマウントされてない？

--- spec/system/home_page_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature "Home", type: :system do
-  scenario "Home page has correct title", js: true do
-    visit "/"
-    expect(page).to have_title "相席アプリ"
-  end
-end
