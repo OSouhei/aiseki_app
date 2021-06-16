bab62f657045f8ff7543d732649feedc85ce4d7f
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-06-16 14:47:01 +0900

    Remove: 不要なjsファイルを削除
      後でVueで実装する

--- app/javascript/packs/flash.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-  const flash = $('.flash');
-  flash.fadeOut(2500);
-})
--- app/javascript/packs/input.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  const input = $('.profile-image-input');
-  input.on('input', function() {
-    const file = $(this).prop('files')[0];
-    $('.filename').text("添付ファイル：" + file.name);
-  });
-})
