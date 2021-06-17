fa8b1c1e7a4c9762edb9bce0cd93e10c582a0835
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-06-17 22:33:57 +0900

    Update: ログイン中のユーザー情報を保持するためのステートを定義

--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -6,17 +6,31 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   strict: true,
   state: {
-    flashMessage: ''
+    flashMessage: '', // flashの文章
+    currentUser: {} // ログイン中のユーザ
+  },
+  getters: {
+    // ユーザーがログインしているか？
+    isLoggedIn(state) {
+      return Object.keys(state.currentUser).length !== 0 ? true : false
+    }
   },
   mutations: {
     // flashメッセージを設定
     setFlashMessage(state, message) {
       state.flashMessage = message
+    },
+    // ログイン中のユーザを設定
+    setCurrentUser(state, user) {
+      state.currentUser = user
     }
   },
   actions: {
     setFlashMessage(context, message) {
       context.commit('setFlashMessage', message)
+    },
+    setCurrentUser(context, user) {
+      context.commit('setCurrentUser', user)
     }
   }
 })
