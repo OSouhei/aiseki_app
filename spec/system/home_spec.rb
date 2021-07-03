152b7f70478666ec369e5a884011eb9af54b7228
OSouhei <68312101+OSouhei@users.noreply.github.com>
2021-07-03 11:20:32 +0900

    Modify: it を scenario に書き換え

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Home page", type: :system do
-  it "has correct title", js: true do
+  scenario "has correct title", js: true do
     visit "/"
     expect(page).to have_title "相席アプリ"
   end
